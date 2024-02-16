ecac02b4cc745f5ebd7b759c956aeec5aaec38c8
Chibgatullah Minhaz <119798464+Chibgatullahminhaz@users.noreply.github.com>
2024-02-16 16:18:49 +0600

    Second Commid

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,4 +9,9 @@ routes.get("/home", (req,res)=>{
     res.status(200).send('<h1> This is home Page <h1/>')
     })
 
+routes.get("/user", (req,res)=>{
+        res.status(200).send('<h1> This is user Page <h1/>')
+        })
+
+
 module.exports = routes;
