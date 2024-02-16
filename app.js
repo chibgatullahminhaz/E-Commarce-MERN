ea6b8a7b4ed8c7e02279c9dc847f0165e20f60f8
Chibgatullah Minhaz <119798464+Chibgatullahminhaz@users.noreply.github.com>
2024-02-16 16:16:02 +0600

    First Day Work

---
+++ app.js
@@ -0,0 +1,25 @@
+require('./config/db')
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes/user.routes'); 
+const app = express();
+// usesing 
+app.use(cors())
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+// routes
+app.use( "/", routes);
+
+
+// not found Router
+app.use( (req, res)=>{
+    res.status('404').send("Page not found");
+  });
+
+//   error server 
+app.use((err,req,res)=>{
+    res.status(500).send(err.message)
+})
+
+
+module.exports = app;
---
+++ routes/user.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = express.Router();
+
+// create home route
+routes.get("/", (req,res)=>{
+res.status(200).send('<h1> This is home Page <h1/>')
+})
+routes.get("/home", (req,res)=>{
+    res.status(200).send('<h1> This is home Page <h1/>')
+    })
+
+module.exports = routes;
